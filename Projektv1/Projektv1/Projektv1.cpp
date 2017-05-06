66fd15e0d93554c507cce8553a99b505e2f0ba39
robertmycka <robertsnk1@interia.pl>
2017-05-06 12:42:06 +0200

    V1 Rysowanie

---
+++ Projektv1/Projektv1/Projektv1.cpp
@@ -0,0 +1,56 @@
+
+#include "stdafx.h"
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv\cv.h>
+#include <sstream>
+#include <string>
+#include <opencv\highgui.h>
+
+using namespace cv;
+using namespace std;
+
+
+
+const int width = 640;
+const int height = 480;
+
+
+
+string intToString(int number){
+
+
+	std::stringstream ss;
+	ss << number;
+	return ss.str();
+}
+
+void Rysowanie(int x, int y, Mat &frame){
+	
+
+
+	circle(frame, Point(x, y), 20, Scalar(0, 255, 0), 2);
+	if (y - 25>0)
+		line(frame, Point(x, y), Point(x, y - 25), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(x, 0), Scalar(0, 255, 0), 2);
+	if (y + 25 <height)
+		line(frame, Point(x, y), Point(x, y + 25), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(x, height), Scalar(0, 255, 0), 2);
+	if (x - 25>0)
+		line(frame, Point(x, y), Point(x - 25, y), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(0, y), Scalar(0, 255, 0), 2);
+	if (x + 25< width)
+		line(frame, Point(x, y), Point(x + 25, y), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(width, y), Scalar(0, 255, 0), 2);
+
+
+	putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Scalar(0, 255, 0), 2);
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
